790781450d12b0bc2532a91b3eb4bf442c3eb1b9
CodingFlamez <Umukoroisreal3@gmail.com>
2022-04-11 10:23:08 -0700

    New Task

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main Entry point
+ *
+ * Return: always 0 (success)
+ */
+
+in main(void)
+{
+	int a, b, c;
+
+	for (a = 0; a < 10; ++a)
+	{
+		for (b = 0; b < 10; ++b)
+		{
+			for (c = 0; c < 10; ++c)
+			{
+				if (c > b > a && a != b != c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+
+					if ((a < 7 || b < 8) || c != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+
+			}
+		}
+	}
+putchar('\n');
+return (0);
+}
